3f2989b38454e4a4634dec91cddd5a35bfe2dfca
Kyle Henry <kylehenry7000@gmail.com>
2024-04-12 09:47:39 -0500

    Added Line to Chart

--- src/BreatheEasySim/Components/Chart.java
+++ src/BreatheEasySim/Components/Chart.java
@@ -12,31 +12,59 @@ import org.jfree.data.xy.XYSeriesCollection;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.data.Range;
+import org.jfree.data.xy.XYDataset;
 
 public class Chart extends JPanel {
-    public Chart() {
-        // Create a dataset (you can replace this with your own data)
-        XYSeries series = new XYSeries("Sample Data");
-        series.add(1, 10);
-        series.add(2, 20);
-        series.add(3, 15);
+    public XYSeries series;
+    public double elapsedSeconds; // Counter for elapsed seconds
+    public double amplitude = 20.0;
 
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(series);
+    public double getAmplitude() {
+        return amplitude;
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public double getPhaseShift() {
+        return phaseShift;
+    }
+
+    public void setAmplitude(double amplitude) {
+        this.amplitude = amplitude;
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+    private double frequency = 2.5; // Hz
+    private double phaseShift = 0;
+
+    public void setPhaseShift(double phaseShift) {
+        this.phaseShift = phaseShift;
+    }
+    
+    public Chart() {
+        super();
+        series = new XYSeries("Random Data");
+        
+        // Pre-populate the series with NaN values
+        for (double i = 0.0; i <= 1200; i++) {
+            double temp = Math.round(i * 0.01 * 100) / 100.0;
+            series.add(temp, Double.NaN);
+        }
+        
+        elapsedSeconds = 0.0; // Initialize elapsed seconds
+        final XYDataset dataset = createDataset();
 
         // Create the chart
-        JFreeChart chart = ChartFactory.createXYLineChart(
-                null, // Chart title
-                null, // X-axis label
-                null, // Y-axis label
-                dataset, // Dataset
-                PlotOrientation.VERTICAL,
-                false, // Include legend
-                false, // Include tooltips
-                false // Include URLs
-        );
+        final JFreeChart chart = createChart(dataset);
 
         // Customize the plot
         XYPlot plot = chart.getXYPlot();
@@ -62,7 +90,55 @@ public class Chart extends JPanel {
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new Dimension(1367, 200)); // Set preferred size
         chartPanel.setOpaque(false);
+        chartPanel.setMouseZoomable(false, false);
         add(chartPanel);
+        
+        // Set up a timer to add new data points every second
+        Timer timer = new Timer(10, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addDataPoint();
+            }
+        });
+        timer.start();
+    }
+    
+    private XYDataset createDataset() {
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+        return dataset;
+    }
+    
+    private JFreeChart createChart(final XYDataset dataset) {
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                null, // Chart title
+                null, // X-axis label
+                null, // Y-axis label
+                dataset, // Dataset
+                PlotOrientation.VERTICAL,
+                false, // Include legend
+                false, // Include tooltips
+                false // Include URLs
+        );
+        
+        return chart;
+    }
+    
+    private void addDataPoint() {
+        double val = Math.round(elapsedSeconds * 100%1200) / 100.0;
+        int index = series.indexOf(val);
+        series.remove(index);
+        double yValue = amplitude * Math.sin(frequency * elapsedSeconds + phaseShift);
+        series.addOrUpdate(val, yValue); // Use modulo operator to reset x to 0 when it exceeds 12
+        elapsedSeconds+=.01; // Increment elapsed seconds
+        
+        // Fix precision issue
+        elapsedSeconds = Math.round(elapsedSeconds * 100) / 100.0;
+        
+
+        // Adjust the range of the x-axis to show only the last 12 seconds
+//        plot.getDomainAxis().setRange(new Range(Math.max(0, elapsedSeconds - 12), elapsedSeconds));
+
     }
 
     public static void main(String[] args) {
